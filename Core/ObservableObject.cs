95cc795dc36bc7524f172ea7e62bcc8920d04603
Wiktoria Szczepańska <87717209+ArcherWike@users.noreply.github.com>
2023-06-27 12:50:56 +0200

    Base GUI

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UiDesign.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanger([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
