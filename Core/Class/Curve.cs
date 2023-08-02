ca18fbbddafe3f356788f8d0117f923937c3c307
Wiktoria Szczepańska <87717209+ArcherWike@users.noreply.github.com>
2023-08-02 13:32:19 +0200

    fix files- Quadratic Bezier Curve from branch

---
+++ Core/Class/Curve.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using UiDesign;
+
+namespace Curves_editor.Core.Class
+{
+    enum pointType
+    {
+        Normal,
+        Control
+    }
+    public class Curve_point
+    {
+        public Point ellipse_positionID { get; set; }
+        public Ellipse ellipseID { get; set; }
+
+        //pointType ellipse_type;
+
+        public Curve_point(Point position, Ellipse ellipse)
+        {
+            ellipse_positionID = position;
+            ellipseID = ellipse; 
+        }
+    }
+
+    
+    public class Segment_curve
+    {
+        PathGeometry myPathGeometry = new PathGeometry();
+        Path segmentPath = new Path();
+
+        public List<Curve_point> points = new List<Curve_point>();
+        public List<Line> lines = new List<Line>();
+
+        private static Point GetCoordToCanvast(Point pointPosition)
+        {
+            Point result = new Point((pointPosition.X * 200) + 40, 800 - pointPosition.Y * 200);
+
+            return result;
+        }
+
+        public Segment_curve(Curve_point start_point, Curve_point end_point)
+        {
+            points.Add(start_point);
+
+            Point newEndPos = GetCoordToCanvast(end_point.ellipse_positionID);
+            Point newStartPos = GetCoordToCanvast(start_point.ellipse_positionID);
+
+            Curve_point control_point = new Curve_point(new Point(
+                start_point.ellipse_positionID.X + (Math.Abs(end_point.ellipse_positionID.X - start_point.ellipse_positionID.X)/2),
+                start_point.ellipse_positionID.Y + (Math.Abs(end_point.ellipse_positionID.Y - start_point.ellipse_positionID.Y)/2)),
+                new Ellipse());
+
+            /*Curve_point control_point = new Curve_point(new Point(
+                Math.Abs(newEndPos.X - newStartPos.X),
+                Math.Abs(newEndPos.Y - newEndPos.Y)),
+                new Ellipse());*/
+
+            points.Add(control_point);
+            
+            points.Add(end_point);
+
+            lines.Add(CreateLine(
+                start_point.ellipse_positionID,
+                control_point.ellipse_positionID));
+            lines.Add(CreateLine(
+                control_point.ellipse_positionID,
+                end_point.ellipse_positionID));
+        }
+        public Path SegmentGet_pathGeometry()
+        {
+            segmentPath.Stroke = Brushes.Black;
+            segmentPath.StrokeThickness = 5;
+
+            if (points.Count() < 4)
+            {
+                BezierSegment new_segment;
+                PathFigure pathFigure = new PathFigure();
+
+                myPathGeometry.Clear();
+                pathFigure.StartPoint = points[0].ellipse_positionID;
+                if (points.Count() == 2)
+                {
+                    Curve_point control_point = new Curve_point(new Point(
+                      Math.Abs(points[1].ellipse_positionID.X - points[0].ellipse_positionID.X),
+                      points[0].ellipse_positionID.Y + Math.Abs(points[1].ellipse_positionID.Y - points[0].ellipse_positionID.Y)),
+                      new Ellipse());
+
+                    new_segment = new BezierSegment(
+                        points[0].ellipse_positionID,
+                        control_point.ellipse_positionID,
+                        points[1].ellipse_positionID,
+                        true);
+                }
+                else
+                {
+                    new_segment = new BezierSegment(
+                        points[0].ellipse_positionID,
+                        points[1].ellipse_positionID,
+                        points[2].ellipse_positionID,
+                        true);
+                }
+                pathFigure.Segments.Add(new_segment);
+                myPathGeometry.Figures.Add(pathFigure);
+
+                segmentPath.Data = myPathGeometry;
+            }
+
+            return segmentPath;
+        }
+
+        public Curve_point GetControlPoint()
+        {
+            return points[1];
+        }
+
+        private Line CreateLine(Point start, Point end)
+        {
+            Line line = new Line();
+
+            line.X1 = start.X;
+            line.Y1 = start.Y;
+            line.X2 = end.X;
+            line.Y2 = end.Y;
+
+            return line;
+        }
+        public List<Line> GetUpdatedLinesArray()
+        {
+            lines[0].X1 = points[0].ellipse_positionID.X;
+            lines[0].Y1 = points[0].ellipse_positionID.Y;
+            lines[0].X2 = points[1].ellipse_positionID.X;
+            lines[0].Y2 = points[1].ellipse_positionID.Y;
+
+            lines[1].X1 = points[1].ellipse_positionID.X;
+            lines[1].Y1 = points[1].ellipse_positionID.Y;
+            lines[1].X2 = points[2].ellipse_positionID.X;
+            lines[1].Y2 = points[2].ellipse_positionID.Y;
+
+            return lines;
+        }
+        private void Create_controlPoint()
+        {
+            
+
+            /*Curve_point end_point = points[points.Count - 1];
+
+            points[points.Count() - 1] = new_control_point;
+            points.Add(end_point);*/
+        }    
+    }
+
+    public class PathEventArgs : EventArgs
+    {
+        public Path pathGeometry { get; private set; }
+        public PathEventArgs(Path path_)
+        {
+            pathGeometry = path_;
+        }
+    }
+
+    public class LineEventArgs : EventArgs
+    {
+        public List<Line> linesArray { get; private set; }
+        public LineEventArgs(List<Line> line_)
+        {
+            linesArray = line_;
+        }
+    }
+
+    public class CurvePointEventArgs : EventArgs
+    {
+        public Curve_point curvePoint { get; private set; }
+        public CurvePointEventArgs(Curve_point point_) 
+        { 
+            curvePoint = point_;
+        }
+    }
+
+    internal class Curve
+    {
+        public List<Curve_point> base_pointArray = new List<Curve_point>();
+        public List<Curve_point> pointArray = new List<Curve_point>();
+        public List<Segment_curve> segmentsArray = new List<Segment_curve>();
+
+        public delegate void PathHandler(object sender, PathEventArgs e);
+        public event PathHandler PathGeomertyAddToViewport;
+
+        public delegate void LineHandler(object sender, LineEventArgs e);
+        public event LineHandler OnLineAdded;
+        public event LineHandler UpdateLine;
+
+        public delegate void CurvePointHandler(object sender, CurvePointEventArgs e);
+        public event CurvePointHandler OnCurvePointAdded;
+
+        public List<Curve_point> SortPoints(List<Curve_point> array)
+        {
+            for (int step = 1; step < array.Count; step++)
+            {
+                Curve_point key = new Curve_point(array[step].ellipse_positionID, array[step].ellipseID);
+                int j = step - 1;
+
+                Point swapPoint = new Point(0, 0);
+
+                while (j >= 0 && key.ellipse_positionID.X < array[j].ellipse_positionID.X)
+                {
+                    swapPoint = new Point(
+                        array[j].ellipse_positionID.X,
+                        array[j].ellipse_positionID.Y);
+
+                    array[j + 1].ellipse_positionID = swapPoint;
+
+                    j--;
+
+                }
+
+                swapPoint = new Point(
+                            key.ellipse_positionID.X,
+                            key.ellipse_positionID.Y);
+
+                array[j + 1].ellipse_positionID = swapPoint;
+
+
+            }
+            return array;
+        }
+        
+
+        public void DeleteCurves()
+        {
+
+            base_pointArray = new List<Curve_point>();
+            segmentsArray = new List<Segment_curve>();
+        }
+
+        private static Point GetCoordToCanvast(Point pointPosition)
+        {
+            Point result = new Point((pointPosition.X * 200) + 40, 800 - pointPosition.Y * 200);
+
+            return result;
+        }
+        
+
+        void AddSegment_to_viewport(Segment_curve segment)
+        {
+            PathEventArgs eventArgs = new PathEventArgs(segment.SegmentGet_pathGeometry());
+            PathGeomertyAddToViewport(this, eventArgs);
+        }
+
+
+        public void AddPoint(Point point, Ellipse ellipse)
+        {
+            Curve_point curve_Point = new Curve_point(point, ellipse);
+            pointArray.Add(curve_Point);
+
+            if (base_pointArray.Count() > 0)
+            {
+                Segment_curve segment_Curve = new Segment_curve(base_pointArray[base_pointArray.Count() - 1], curve_Point);
+                segmentsArray.Add(segment_Curve);
+
+                LineEventArgs eventArgs = new LineEventArgs(segment_Curve.GetUpdatedLinesArray());
+                OnLineAdded(this, eventArgs);
+
+                CurvePointEventArgs eventArgs1 = new CurvePointEventArgs(segment_Curve.GetControlPoint());
+                OnCurvePointAdded(this, eventArgs1);
+                pointArray.Add(eventArgs1.curvePoint);
+
+                AddSegment_to_viewport(segment_Curve);
+            }
+
+            base_pointArray.Add(curve_Point);
+        }
+
+        private void OnSegmentLineAdded(object sender, LineEventArgs e)
+        {
+           
+        }
+
+        public void UpdatePointPosition(Ellipse sender, Point e)
+        {
+            foreach (Curve_point curve_Point in pointArray)
+            {
+                if (sender == curve_Point.ellipseID)
+                {
+                    curve_Point.ellipse_positionID = GetCoordToCanvast(e);
+
+                    break;
+                }
+            }
+            foreach (Segment_curve segment in segmentsArray)
+            {
+                for (int i = 0; i < segment.points.Count; i++)
+                {
+                    if (segment.points[i].ellipseID == sender)
+                    {
+                        AddSegment_to_viewport(segment);
+
+                        LineEventArgs eventArgs = new LineEventArgs(segment.GetUpdatedLinesArray());
+                        //OnLineAdded(this, eventArgs);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,251 @@
+﻿using Curves_editor.Core.Class;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UiDesign
+{
+    public partial class MainWindow : Window
+    {
+        Path myPath = new Path();
+        Ellipse activepoint = null;
+
+        Ellipse circle_point = new Ellipse();
+        Point CirclePoint = new Point(190, 120);
+        MovableCirclePoint movablePoint;
+
+        Curve curve = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Create_circle_point();
+            movablePoint = new MovableCirclePoint(CirclePoint, circle_point, this);
+        }
+
+        private void Create_circle_point()
+        {
+            circle_point.Stroke = Brushes.Red;
+            circle_point.Fill = System.Windows.Media.Brushes.Red;
+            circle_point.StrokeThickness = 10;
+            circle_point.Width = 30;
+            circle_point.Height = 30;
+            SettingsWindow.Children.Add(circle_point);
+        }
+
+        private Ellipse CreatePoint(Point mousePosition)
+        {
+            Ellipse myEllipse = new Ellipse();
+
+            myEllipse.Width = 30;
+            myEllipse.Height = 30;
+            myEllipse.Fill = System.Windows.Media.Brushes.White;
+            CordSys.Children.Add(myEllipse);
+            SetPointPosition(myEllipse, mousePosition);
+            myEllipse.MouseEnter += Ellipse_mouseEnter;
+            myEllipse.MouseLeave += Ellipse_mouseLeave;
+            myEllipse.MouseLeftButtonDown += MyEllipse_MouseLeftButtonDown;
+            myEllipse.MouseLeftButtonUp += MyEllipse_MouseLeftButtonUp;
+
+            return myEllipse;
+        }
+
+
+
+        public void SetPointPosition(Ellipse myEllipse, Point mousePoint)
+        {
+            if (curve != null)
+            {
+                Point delta = new Point((mousePoint.X - 15 - Canvas.GetLeft(myEllipse)) / 200,
+                     (Canvas.GetTop(myEllipse) - mousePoint.Y + 15) / 200);
+
+                Point new_pos = GetCanvastToCoord(mousePoint);
+                curve.UpdatePointPosition(myEllipse, new_pos);
+            }
+
+            Canvas.SetLeft(myEllipse, mousePoint.X - 15);
+            Canvas.SetTop(myEllipse, mousePoint.Y - 15);
+        }
+
+        public void SetControlPointPosition(Ellipse myEllipse, Point mousePoint)
+        {
+            if (curve != null)
+            {
+                Point delta = new Point((mousePoint.X - 10 - Canvas.GetLeft(myEllipse)) / 200,
+                     (Canvas.GetTop(myEllipse) - mousePoint.Y + 10) / 200);
+
+                Point new_pos = GetCanvastToCoord(mousePoint);
+                curve.UpdatePointPosition(myEllipse, new_pos);
+            }
+
+            Canvas.SetLeft(myEllipse, mousePoint.X - 15);
+            Canvas.SetTop(myEllipse, mousePoint.Y - 15);
+        }
+
+        private void MyEllipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            activepoint = null;
+            e.Handled = true;
+        }
+
+        private void MyEllipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            activepoint = (sender as Ellipse);
+        }
+
+        public static Point GetCanvastToCoord(Point mousePosition)
+        {
+            Point result = new Point((mousePosition.X - 40) / 200, 4 - (mousePosition.Y / 200));
+
+            return result;
+        }
+        public static Point GetCoordToCanvast(Point pointPosition)
+        {
+            Point result = new Point((pointPosition.X * 200) + 40, 800 - pointPosition.Y * 200);
+
+            return result;
+        }
+
+        private void Ellipse_mouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Ellipse).Fill = System.Windows.Media.Brushes.Red;
+        }
+        private void Ellipse_mouseLeave(object sender, MouseEventArgs e)
+        {
+            if ((sender as Ellipse).Width == 20)
+            {
+                (sender as Ellipse).Fill = System.Windows.Media.Brushes.Blue;
+            }
+            else
+            {
+                (sender as Ellipse).Fill = System.Windows.Media.Brushes.White;
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+
+            if (curve == null)
+            {
+                curve = new Curve();
+                dataGrid.ItemsSource = curve.base_pointArray;
+
+                //curve.UpdateSegment += new Curve.SegmentHandler(SegmentViewUpdate);
+                curve.PathGeomertyAddToViewport += new Curve.PathHandler(UpdateSegmentViewport);
+
+                curve.OnLineAdded += new Curve.LineHandler(CreateLine);
+                curve.UpdateLine += new Curve.LineHandler(UpdateLine);
+                curve.OnCurvePointAdded += new Curve.CurvePointHandler(CreateCotrolPoint);
+            }
+            if (activepoint == null) ///////////////##
+            {
+
+                
+                
+/*                else
+                {
+                    curve.AddPoint((
+                e.GetPosition(this.CordSys)),
+                CreateControlPoint(e.GetPosition(this.CordSys)));
+                }*/
+
+            }
+            curve.AddPoint((
+                    e.GetPosition(this.CordSys)),
+                    CreatePoint(e.GetPosition(this.CordSys)));
+        }
+
+        private void CreateCotrolPoint(object sender, CurvePointEventArgs e)
+        {
+
+            e.curvePoint.ellipseID.Width = 20;
+            e.curvePoint.ellipseID.Height = 20;
+            e.curvePoint.ellipseID.Fill = System.Windows.Media.Brushes.SpringGreen;
+            CordSys.Children.Add(e.curvePoint.ellipseID);
+            SetControlPointPosition(e.curvePoint.ellipseID, (e.curvePoint.ellipse_positionID));
+            e.curvePoint.ellipseID.MouseEnter += Ellipse_mouseEnter;
+            e.curvePoint.ellipseID.MouseLeave += Ellipse_mouseLeave;
+            e.curvePoint.ellipseID.MouseLeftButtonDown += MyEllipse_MouseLeftButtonDown;
+            e.curvePoint.ellipseID.MouseLeftButtonUp += MyEllipse_MouseLeftButtonUp;
+
+        }
+
+        private void UpdateLine(object sender, LineEventArgs e)
+        {
+            
+        }
+
+        private void CreateLine(object sender, LineEventArgs e)
+        {
+            foreach (Line line in e.linesArray)
+            {
+                line.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+                CordSys.Children.Add(line);
+            }
+        }
+
+        private void UpdateSegmentViewport(object sender, PathEventArgs e)
+        {
+
+            
+
+            CordSys.Children.Remove(e.pathGeometry);
+            /*myPath = new Path();
+            myPath.Stroke = Brushes.Black;
+            myPath.StrokeThickness = 1;
+            myPath.Data = e.pathGeometry;*/
+
+            e.pathGeometry.MouseLeftButtonDown += MypathGeometry_MouseLeftButtonDown;
+            e.pathGeometry.MouseLeftButtonUp += MypathGeometryLeftButtonUp;
+
+            CordSys.Children.Add(e.pathGeometry);
+
+        }
+
+        private void MypathGeometryLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Path).Stroke = Brushes.Black;
+        }
+
+        private void MypathGeometry_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Path).Stroke = Brushes.Yellow;
+        }
+
+        private void CordSys_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (activepoint != null)
+            {
+                SetPointPosition(activepoint, e.GetPosition(this.CordSys));
+                dataGrid.Items.Refresh();
+            }
+        }
+
+
+        private void Clear_Viewport(object sender, RoutedEventArgs e)
+        {
+            curve = null;
+            CordSys.Children.Clear();
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Refresh();
+            
+        }
+    }
+}
